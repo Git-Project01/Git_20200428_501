b84ff827dab4a67d8cebdb5715d4de3a102cc492
Git-Project01 <git_project1@qq.com>
2020-04-28 09:37:51 +0800

    修改文件 Student.java 添加toString()方法,setAge赋值注释

--- Student.java
+++ Student.java
@@ -13,11 +13,14 @@ public class Student{
 	}
 
 	public void setAge(Integer age){
-		this.age = age;
+		this.age = age;			//edit by Git-Project01
 	}
 
 	public Integer getAge(){
 		return this.age;
 	}
 
+	public String toString(){
+		return "Student [name = "+this.name+",age = "+this.age+"]";
+	}
 }
