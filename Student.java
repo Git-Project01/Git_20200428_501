0881b18b5e01702d183c34583700138b195e47b4
Git-Project02 <2252150995@qq.com>
2020-04-28 09:03:08 +0800

    修改文件 Student.java 添加age属性,添加setName(),getName()方法

--- Student.java
+++ Student.java
@@ -1,11 +1,21 @@
 public class Student{
 	private String name;
 
+	private Integer age;
+
 	public void setName(String name){
 		this.name = name;
 	}
 
 	public String getName(){
 		return this.name;
+	}
+
+	public void setAge(Integer age){
+		this.age = age;
+	}
+
+	public Integer getAge(){
+		return this.age;
 	}
 }
